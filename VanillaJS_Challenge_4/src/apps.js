87ec85217694adf1a63d11fec007b091d16e2ab4
Kh7ae <102511724+Kh7ae@users.noreply.github.com>
2022-08-10 23:59:39 +0900

    Update

--- VanillaJS_Challenge_4/src/apps.js
+++ VanillaJS_Challenge_4/src/apps.js
@@ -1,9 +1,19 @@
 import "./styles.css";
 
+const clock = document.querySelector(".js-clock");
+
 function getTime() {
-}
+    const dDate = new Date("2022-12-24");
+    const nowDate = new Date();
+    const diff = dDate - nowDate
 
-function init() {
-}
+    const diffDay = String(Math.floor(diff / (1000*60*60*24))).padStart(2, "0");
+    const diffHour = String(Math.floor((diff / (1000*60*60)) % 24)).padStart(2, "0");
+    const diffMin = String(Math.floor((diff / (1000*60)) % 60)).padStart(2, "0");
+    const diffSec = String(Math.floor(diff / 1000 % 60)).padStart(2, "0");
 
-init();
+    clock.innerText = `${diffDay}d ${diffHour}h ${diffMin}m ${diffSec}s`
+};
+
+getTime();
+setInterval(getTime, 1000);
