2de200cd5f58b0539ce36017f59976bf083a2e89
Kh7ae <102511724+Kh7ae@users.noreply.github.com>
2022-08-11 01:29:58 +0900

    Update

--- VanillaJS_Challenge_5/src/apps.js
+++ VanillaJS_Challenge_5/src/apps.js
@@ -1,6 +1,17 @@
 import "./styles.css";
+const selector = document.querySelector(".js-select");
+
+
+function saveCountries() {
+    localStorage.setItem("country", selector.value)
+}
+selector.addEventListener("input", saveCountries)
 
 function loadCountries() {
+    const loadVal = localStorage.getItem("country");
+    if (loadVal !== null) {
+        selector.value = loadVal
+    }
 }
 
 loadCountries();
