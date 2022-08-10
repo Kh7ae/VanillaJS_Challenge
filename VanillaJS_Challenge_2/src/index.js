4a4bedf6c2469d87ba5acf01f1702a75430d7732
Kh7ae <102511724+Kh7ae@users.noreply.github.com>
2022-08-10 21:42:44 +0900

    Update

--- VanillaJS_Challenge_2/src/index.js
+++ VanillaJS_Challenge_2/src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+const body = document.querySelector("body");
+const colors = ["#2c8bd4", "#8f46ab", "#ebbb08"];
+
+const superEventHandler = {
+    handleChaingingColor: function() {
+        const windowWidth = window.innerWidth;
+        const ONEONE = screen.width;
+        const ONETWO = ONEONE * (2.3/3);
+        const ONETHREE = ONEONE * (1.3/3);
+
+        if (windowWidth <= ONETHREE) {
+            body.style.backgroundColor = colors[0];
+        } else if (windowWidth > ONETHREE && windowWidth <= ONETWO) {
+            body.style.backgroundColor = colors[1];
+        } else {
+            body.style.backgroundColor = colors[2];
+        }
+    },
+};
+
+window.addEventListener("resize", superEventHandler.handleChaingingColor);
