97573401f5e4f6f2baab0316447c61d7afffa0ea
Kh7ae <102511724+Kh7ae@users.noreply.github.com>
2022-08-10 21:27:49 +0900

    Update

---
+++ VanillaJS_Challenge_3/README.md
@@ -0,0 +1,2 @@
+# VanillaJS_Challenge_3
+ 
---
+++ VanillaJS_Challenge_3/src/apps.js
@@ -0,0 +1,14 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+function hadnleChange(event) {
+}
+
+function handleSubmit(event) {
+}
+
+function init() {
+}
+
+init();
