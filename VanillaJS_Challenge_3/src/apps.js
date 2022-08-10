d18d77f3a71f5952b0b8cc8e32ae45767f04585a
Kh7ae <102511724+Kh7ae@users.noreply.github.com>
2022-08-10 23:45:33 +0900

    Update

--- VanillaJS_Challenge_3/src/apps.js
+++ VanillaJS_Challenge_3/src/apps.js
@@ -2,13 +2,35 @@
 import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 
-function hadnleChange(event) {
+const maxNum = document.querySelector("h4 span");
+const slider = document.querySelector(".jsRange");
+const inputNum = document.querySelector(".guessField");
+const submitBtn = document.querySelector(".guessSubmit");
+const result = document.querySelector(".result");
+const winOrLose = document.querySelector(".winOrLose");
+
+function handleChange() {
+    maxNum.innerText = slider.value;
 }
 
-function handleSubmit(event) {
+function handleSubmit() {
+    const inputNumVal = inputNum.value;
+    const machineNumVal = String(Math.floor(Math.random() * slider.value));
+    result.innerText = `You chose ${inputNumVal}, the machine chose ${machineNumVal}`;
+
+    if (inputNumVal === machineNumVal) {
+        winOrLose.innerText = "You won!"
+    } else {
+        winOrLose.innerText = "You lose!"
+    }
 }
 
-function init() {
-}
-
-init();
+slider.addEventListener("input", handleChange);
+// click, enter 이벤트 등록
+submitBtn.addEventListener("click", handleSubmit);
+inputNum.addEventListener('keypress', (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        handleSubmit();
+    }
+});
